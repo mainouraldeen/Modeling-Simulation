0286b0c9e684d6344729c5b919f73dbb97bd6865
Nourhan Emad <nourhan.3mad@gmail.com>
2019-11-15 17:46:00 +0200

    bearing, current table wrong output

--- BearingMachineSimulation/BearingMachineSimulation/Form1.Designer.cs
+++ BearingMachineSimulation/BearingMachineSimulation/Form1.Designer.cs
@@ -28,11 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.readFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // readFileButton
+            // 
+            this.readFileButton.Location = new System.Drawing.Point(184, 179);
+            this.readFileButton.Name = "readFileButton";
+            this.readFileButton.Size = new System.Drawing.Size(113, 60);
+            this.readFileButton.TabIndex = 0;
+            this.readFileButton.Text = "Read data";
+            this.readFileButton.UseVisualStyleBackColor = true;
+            this.readFileButton.Click += new System.EventHandler(this.readFileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 460);
+            this.Controls.Add(this.readFileButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button readFileButton;
     }
 }
--- BearingMachineSimulation/BearingMachineSimulation/Form1.cs
+++ BearingMachineSimulation/BearingMachineSimulation/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BearingMachineTesting;
+using BearingMachineModels;
+using System.IO;
 
 namespace BearingMachineSimulation
 {
@@ -16,6 +18,147 @@ namespace BearingMachineSimulation
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static SimulationSystem simSys = new SimulationSystem();
+
+        public void fillSimSysObj(string[] sysData)
+        {
+            simSys.DowntimeCost = Convert.ToInt32(sysData[1]);
+            simSys.RepairPersonCost = Convert.ToInt32(sysData[4]);
+            simSys.BearingCost = Convert.ToInt32(sysData[7]);
+            simSys.NumberOfHours = Convert.ToInt32(sysData[10]);
+            simSys.NumberOfBearings = Convert.ToInt32(sysData[13]);
+            simSys.RepairTimeForOneBearing = Convert.ToInt32(sysData[16]);
+            simSys.RepairTimeForAllBearings = Convert.ToInt32(sysData[19]);
+            int i = 22;
+            while (sysData[i] != "")
+            {
+                string[] line = sysData[i].Split(new string[] { ", " }, StringSplitOptions.None);
+                TimeDistribution delayTimeDistribution = new TimeDistribution();
+                delayTimeDistribution.Time = Convert.ToInt32(line[0]);
+                delayTimeDistribution.Probability = Convert.ToDecimal(line[1]);
+                simSys.DelayTimeDistribution.Add(delayTimeDistribution);
+                i++;
+            }
+            i+=2;
+            while (i != sysData.Length)
+            {
+                string[] line = sysData[i].Split(new string[] { ", " }, StringSplitOptions.None);
+                TimeDistribution bearingTimeDistribution = new TimeDistribution();
+                bearingTimeDistribution.Time = Convert.ToInt32(line[0]);
+                bearingTimeDistribution.Probability = Convert.ToDecimal(line[1]);
+                simSys.BearingLifeDistribution.Add(bearingTimeDistribution);
+                i++;
+            }
+        }
+
+        public void fillDelayTimeDistributionTable()
+        {
+            simSys.DelayTimeDistribution[0].CummProbability = simSys.DelayTimeDistribution[0].Probability;
+            simSys.DelayTimeDistribution[0].MinRange = 1;
+            simSys.DelayTimeDistribution[0].MaxRange = Convert.ToInt32(simSys.DelayTimeDistribution[0].Probability * 100);
+            for (int i = 1; i < simSys.DelayTimeDistribution.Count; i++)
+            {
+                simSys.DelayTimeDistribution[i].CummProbability = simSys.DelayTimeDistribution[i].Probability +
+                    simSys.DelayTimeDistribution[i - 1].CummProbability;
+                simSys.DelayTimeDistribution[i].MinRange = simSys.DelayTimeDistribution[i - 1].MaxRange + 1;
+                simSys.DelayTimeDistribution[i].MaxRange = simSys.DelayTimeDistribution[i - 1].MaxRange +
+                    Convert.ToInt32(simSys.DelayTimeDistribution[i].Probability * 100);
+            }
+        }
+
+        public void fillBearingLifeDistributionTable()
+        {
+            simSys.BearingLifeDistribution[0].CummProbability = simSys.BearingLifeDistribution[0].Probability;
+            simSys.BearingLifeDistribution[0].MinRange = 1;
+            simSys.BearingLifeDistribution[0].MaxRange = Convert.ToInt32(simSys.BearingLifeDistribution[0].Probability * 100);
+            for (int i = 1; i < simSys.BearingLifeDistribution.Count; i++)
+            {
+                simSys.BearingLifeDistribution[i].CummProbability = simSys.BearingLifeDistribution[i].Probability +
+                    simSys.BearingLifeDistribution[i - 1].CummProbability;
+                simSys.BearingLifeDistribution[i].MinRange = simSys.BearingLifeDistribution[i - 1].MaxRange + 1;
+                simSys.BearingLifeDistribution[i].MaxRange = simSys.BearingLifeDistribution[i - 1].MaxRange +
+                    Convert.ToInt32(simSys.BearingLifeDistribution[i].Probability * 100);
+
+            }
+        }
+        public int totalDelay = 0;
+        public void fillCurrentSimlationTable()
+        {
+            CurrentSimulationCase currentSimulation;
+            Random randomNum = new Random();
+            for (int j = 0; j <= simSys.NumberOfBearings; j++)
+            {
+                int currentAccumelatedHours = 0;
+                Bearing bearing = new Bearing();
+                bearing.Index = j+1;
+                while (true)
+                {
+                    //////////////////////////////////////////////Hours//////////////////////////////////////////////
+                    currentSimulation = new CurrentSimulationCase();
+                    bearing.RandomHours = randomNum.Next(1, 101);
+                    for (int i = 0; i < simSys.BearingLifeDistribution.Count(); i++)
+                    {
+                        if (bearing.RandomHours >= simSys.BearingLifeDistribution[i].MinRange && bearing.RandomHours <= simSys.BearingLifeDistribution[i].MaxRange)
+                        {
+                            bearing.Hours = simSys.BearingLifeDistribution[i].Time;
+                            break;
+                        }
+                    }
+                    currentAccumelatedHours += bearing.Hours;
+                    currentSimulation.Bearing = bearing;
+                    currentSimulation.AccumulatedHours = currentAccumelatedHours;
+
+                    /////////////////////////////////////////////////Delay/////////////////////////////////////////
+                    currentSimulation.RandomDelay = randomNum.Next(1, 100);
+                    for (int i = 0; i < simSys.DelayTimeDistribution.Count(); i++)
+                    {
+                        if (currentSimulation.Delay >= simSys.DelayTimeDistribution[i].MinRange && currentSimulation.Delay <= simSys.DelayTimeDistribution[i].MaxRange)
+                        {
+                            currentSimulation.Delay = simSys.DelayTimeDistribution[i].Time;
+                            break;
+                        }
+                    }   
+                 
+                    totalDelay += currentSimulation.Delay;
+                    simSys.CurrentSimulationTable.Add(currentSimulation);
+                    if (currentAccumelatedHours >= simSys.NumberOfHours)
+                        break;
+                }
+            }                       
+
+        }
+
+        public void calcCurrentPerformance()
+        {
+            PerformanceMeasures currentPerformance = new PerformanceMeasures();
+            int totalNumOfBearigns = simSys.CurrentSimulationTable.Count;
+            currentPerformance.BearingCost = totalNumOfBearigns * simSys.BearingCost;
+            currentPerformance.DelayCost = totalDelay * simSys.DowntimeCost;
+            currentPerformance.DowntimeCost = totalNumOfBearigns * simSys.RepairTimeForOneBearing * simSys.DowntimeCost;
+            currentPerformance.RepairPersonCost = (totalNumOfBearigns * simSys.RepairTimeForOneBearing * simSys.RepairPersonCost) / 60;
+            currentPerformance.TotalCost = currentPerformance.BearingCost + currentPerformance.DelayCost + currentPerformance.DowntimeCost + currentPerformance.RepairPersonCost;
+            simSys.CurrentPerformanceMeasures = currentPerformance;
+        }
+        private void readFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                string[] fileData = File.ReadAllLines(fileName);
+                fillSimSysObj(fileData);
+                fillDelayTimeDistributionTable();
+                fillBearingLifeDistributionTable();
+                fillCurrentSimlationTable();
+                calcCurrentPerformance();
+
+                string testing = TestingManager.Test(simSys, Constants.FileNames.TestCase1);
+                MessageBox.Show(testing);
+
+            }
         }
     }
 }
